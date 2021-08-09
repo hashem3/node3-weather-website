b99842a6623553ec2cef92bf04b1b08e463d8c17
hashem3 <hashem.jamal72@gmail.com>
2021-08-10 00:15:39 +0200

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 //console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=35394').then((response) => {
+fetch('/weather?address=35394').then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error)
